00079ea37d6607b508b97ba43078c31da6d5e28c
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-03-28 01:07:04 +0300

    added item controller

---
+++ RankingApp/Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RankingApp.Models;
+
+namespace RankingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private static readonly IEnumerable<ItemModel> Items = new[]
+        {
+            new ItemModel{Id =1, Title = "The Godfather", ImageId=1, Ranking=0,ItemType=1 },
+            new ItemModel{Id =2, Title = "Highlander", ImageId=2, Ranking=0,ItemType=1 }
+        };
+
+        [HttpGet("{itemType:int}")]
+        public ItemModel[] Get(int itemType)
+        {
+            ItemModel[] items = Items.Where(i => i.ItemType == itemType).ToArray();
+            return items;
+        }
+    }   
+}
