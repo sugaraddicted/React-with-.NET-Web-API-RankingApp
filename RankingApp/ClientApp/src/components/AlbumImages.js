3bf0336b8fad8971c8781e77e8934b93b1628a4f
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-04-01 23:39:15 +0300

    added album image component

---
+++ RankingApp/ClientApp/src/components/AlbumImages.js
@@ -0,0 +1,9 @@
+﻿import BlueBanisters from '../Images/Albums/BlueBanisters.png';
+import SCRAPS from '../Images/Albums/SCRAPS.png';
+
+const AlbumImageArr = [
+    { id: 1, image: BlueBanisters },
+    { id: 2, image: SCRAPS }
+];
+
+export default AlbumImageArr;
